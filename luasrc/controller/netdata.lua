738d1c2fecc4bc34812e55c2286ae148a8e05744
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2021-06-03 23:27:45 +0800

    升级 汉化1.30.1

--- luasrc/controller/netdata.lua
+++ luasrc/controller/netdata.lua
@@ -1,15 +1,12 @@
 module("luci.controller.netdata", package.seeall)
 
 function index()
+	if not (luci.sys.call("pidof netdata > /dev/null") == 0) then
+		return
+	end
 	local fs = require "nixio.fs"
 
 	entry({"admin","status","netdata"},template("netdata"),_("NetData"),10).leaf=true
-	entry({"admin", "status", "netdata", "call"}, post("action_netdata"))	
 
 
-end
-
-function action_netdata()
-	luci.sys.call("/usr/share/netdatacn/netdatacn.sh 2>&1 >/dev/null")
-	luci.http.redirect(luci.dispatcher.build_url("admin","status","netdata"))
 end
